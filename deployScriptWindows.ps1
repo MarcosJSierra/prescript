62d7c9d99ed78a85d246d9b46c2a4fb2cd666846
Marcos Sierra <marcosjspac@gmail.com>
2023-03-21 16:05:22 -0600

    cambios en script de windows

--- deployScriptWindows.ps1
+++ deployScriptWindows.ps1
@@ -1,3 +1,6 @@
+chocolatey install microsoft-openjdk17 -y
 chocolatey install maven -y
 chocolatey install tomcat -y
-chocolatey install microsoft-openjdk17 -y
+
+Start-Process PowerShell -Verb runAs -ArgumentList '-comand Copy-Item -force C:\prescript\configDocs\tomcat-users.xml -Destination C:\ProgramData\Tomcat9\conf'
+Start-Process PowerShell -Verb runAs -ArgumentList '-comand Copy-Item -force C:\prescript\configDocs\web.xml -Destination C:\ProgramData\Tomcat9\conf'
